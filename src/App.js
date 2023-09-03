146b965b1002031441be7bbef782f2021b58511b
burhan <alacaburhan122016@gmail.com>
2023-09-04 00:31:18 +0300

    math.floor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(10);
+  const [num, setNum] = useState(5);
+
+  const giveRandom=()=>{
+  setNum(Math.floor(Math.random()* (max-min +1)))
+  }
   return (
     <div className="app">
     <div className='container'>
     <div className='randomNum'>
-    <p>Random Number:  <span>5</span> </p>
+    <p>Random Number:  <span>{num}</span> </p>
     </div>
     <div className='numContainer'>
      <div>
       <p>Min:</p>
-      <input type="number" />
+      <input type="number" value={min} onChange={e=>setMin(e.target.value)} />
      
      </div>
      <div>
       <p>Max:</p>
-      <input type="number" />
+      <input type="number" value={max} onChange={e=>setMax(e.target.value)}/>
      
      </div>
     </div>
-    <button>Get Random Number</button>
+    <button onClick={giveRandom}>Get Random Number</button>
       </div>
 
     </div>
